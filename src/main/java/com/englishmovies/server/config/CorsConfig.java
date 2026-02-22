d2f544064f78b5de299d3adacf7b7e192fc3d90d
Alexander Shabanyan <ashabanyan@phoenixit.ru>
2026-02-22 23:32:07 +0300

    commit

--- src/main/java/com/englishmovies/server/config/CorsConfig.java
+++ src/main/java/com/englishmovies/server/config/CorsConfig.java
@@ -10,7 +10,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns(
+                        "https://ui-sandy-tau.vercel.app",
+                        "http://localhost:*",
+                        "http://127.0.0.1:*"
+                )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
