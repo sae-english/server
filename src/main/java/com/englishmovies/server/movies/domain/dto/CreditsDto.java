23582ab3815771d748053a910199279e580d6442
Alexander Shabanyan <ashabanyan@phoenixit.ru>
2026-03-13 16:42:43 +0300

    commit

--- src/main/java/com/englishmovies/server/movies/domain/dto/CreditsDto.java
+++ src/main/java/com/englishmovies/server/movies/domain/dto/CreditsDto.java
@@ -5,11 +5,15 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 /** Script credits (writtenBy, storyBy, directedBy, source, scriptDate). */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CreditsDto {
-    private final String writtenBy;
-    private final String storyBy;
-    private final String directedBy;
-    private final String source;
-    private final String scriptDate;
+    private String writtenBy;
+    private String storyBy;
+    private String directedBy;
+    private String source;
+    private String scriptDate;
+
+    public CreditsDto() {
+        // default constructor for Jackson
+    }
 
     public CreditsDto(String writtenBy, String storyBy, String directedBy, String source, String scriptDate) {
         this.writtenBy = writtenBy;
@@ -20,8 +24,17 @@ public class CreditsDto {
     }
 
     public String getWrittenBy() { return writtenBy; }
+    public void setWrittenBy(String writtenBy) { this.writtenBy = writtenBy; }
+
     public String getStoryBy() { return storyBy; }
+    public void setStoryBy(String storyBy) { this.storyBy = storyBy; }
+
     public String getDirectedBy() { return directedBy; }
+    public void setDirectedBy(String directedBy) { this.directedBy = directedBy; }
+
     public String getSource() { return source; }
+    public void setSource(String source) { this.source = source; }
+
     public String getScriptDate() { return scriptDate; }
+    public void setScriptDate(String scriptDate) { this.scriptDate = scriptDate; }
 }
